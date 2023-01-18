21f9ee4b6e1a66a2840579f1cf9b3c6ef0081096
Greatmagic1 <123013821+Greatmagic1@users.noreply.github.com>
2023-01-18 23:30:33 +0500

    Firstcommit
    Added new commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 102;
+        int b = 154;
+
+        int c = a + b;
+        System.out.println("Сложение а и б составляет = " + c);
+
+    }
+
+}
